8e719132974b6aec17ce7fa109f0ea4cbc40c39e
vagrant <vagrant@vagrant.vm>
2021-03-29 21:03:38 +0000

    fizz buzz problem 412

--- problems.py
+++ problems.py
@@ -55,3 +55,67 @@ class Solution:
         else:    
             return False
                
+runtime = 0(n)
+
+
+Given the root of a binary tree, return its maximum depth.
+
+A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+ 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        count_right = 0 
+        count_left = 0
+
+        if root is None:
+            return 0
+
+
+
+
+        count_right = self.maxDepth(root.right) + 1 
+        count_left = self.maxDepth(root.left ) + 1
+        
+        if count_right > count_left:
+            return count_right
+        else:
+            return count_left
+
+
+
+Write a program that outputs the string representation of numbers from 1 to n.
+
+But for multiples of three it should output “Fizz” instead of the number and for the multiples of five output “Buzz”. For numbers which are multiples of both three and five output “FizzBuzz”.
+
+Example:
+
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+
+        
+        
+# if multiple of 3 output is "FIZZ"
+# if multiplke of 5 output is "BUZZ"
+# If multiple of 3 & 5 (15) output "fizzbuzz"
+
+        result = []
+
+        for num in range(1, n+1):
+
+            if num % 15 == 0:
+                result.append("FizzBuzz")
+            elif num % 3 == 0:
+                result.append("Fizz")
+            elif num % 5 == 0:
+                result.append("Buzz")
+            else:
+                result.append(str(num))
+        return result 
+                    
