01c42262bd434ee36be69edb4ff24d7cc85cf33c
vagrant <vagrant@vagrant.vm>
2021-03-29 21:55:09 +0000

    reversed string problem 334

--- problems.py
+++ problems.py
@@ -118,4 +118,26 @@ class Solution:
             else:
                 result.append(str(num))
         return result 
-                    
+
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        
+        """
+#         L = len(s) 
+#         #take the string of letters in a list and reverse the order 
+#         #start one from leeft and swap values 
+         
+#         for i in range(L/2):
+#             s[i], s[L-i-1] = s[L-i-1], s[i]
+      
+            
+        left, right = 0, len(s) - 1
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left, right = left + 1, right - 1
+
+
+    or s.reverse()
