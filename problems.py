48bef93e3a1c1e434bd80340b0d0ce0a0de13842
vagrant <vagrant@vagrant.vm>
2021-04-05 02:39:33 +0000

    stacked queued

--- problems.py
+++ problems.py
@@ -196,3 +196,84 @@ class Solution(object):
             else:
                 two_sum_dict[num] = i
         return []
+
+
+class MyQueue:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.queue1 = []
+        self.queue2 = []
+
+    def push(self, x: int) -> None:
+        """
+        Push element x to the back of queue.
+        """
+        
+        self.queue1.append(x)
+        
+        
+    def pop(self) -> int:
+        """
+        Removes the element from in front of queue and returns that element.
+        """
+        while len(self.queue1) != 0:
+        
+            x = self.queue1.pop(-1)
+            
+            self.queue2.append(x)
+    
+        y = self.queue2.pop(-1)
+        while len(self.queue2) != 0:
+            
+            x = self.queue2.pop(-1)
+            
+            self.queue1.append(x)
+            
+        
+        return y 
+    
+            
+            
+
+        
+    def peek(self) -> int:
+        """
+        Get the front element.
+        """
+        while len(self.queue1) != 0:
+           
+            x = self.queue1.pop(-1)
+            
+            self.queue2.append(x)
+        y = self.queue2[-1]
+        while len(self.queue2) != 0:
+            
+            x = self.queue2.pop(-1)
+            
+            self.queue1.append(x)
+            
+          
+        return y    
+    
+            
+
+    def empty(self) -> bool:
+        """
+        Returns whether the queue is empty.
+        
+        """
+        if len(self.queue1) == 0 and len(self.queue2) == 0:
+            return True 
+        return False
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
+
